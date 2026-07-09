6db0492a50c881ebee6a01f0f7ec44ac9d8b8164
DavidMaldonado0828 <lm271022@gmail.com>
2026-07-09 15:09:50 -0500

    refactor(generator): compute latency only for selected mode

--- generator_logs.py
+++ generator_logs.py
@@ -18,6 +18,12 @@ def generate_log(identify_number:int, timestamp:datetime):
     channel = ["APP","SUC","PSE","ATM","CB"]
     opperation_type = ["BALANCE_INQUIRY","TRANSFER","PAYMENT","WITHDRAWAL"]
     response_code = ["00","51","91","96","68"]
+    performance = ["NORMAL_MODE","SLOW_MODE"]
+    mode = choices(performance,weights=[95,5],k=1)[0]
+    if mode == "NORMAL_MODE":
+        latency_ms = int(gauss(250,70))
+    else:
+        latency_ms = int(gauss(2200,400))
     log = {
             "id_transaction" : f"TX-{identify_number:06d}",
             "timestamp" : timestamp.isoformat(),
@@ -25,7 +31,7 @@ def generate_log(identify_number:int, timestamp:datetime):
             "operation_type" : choices(opperation_type,weights=[40,35,20,5],k=1)[0],
             "response_code" : choices(response_code,weights=[95,2,1,1,1],k=1)[0],
             "amount": randint(10000,3000000),
-            "latency_ms" : round(gauss(200,30),3)
+            "latency_ms" : latency_ms
     }
     print(json.dumps(log,indent = 2,ensure_ascii=False))
 
@@ -34,5 +40,4 @@ timestamp = datetime(2026,7,8,8,34,56)
 for i in range(register_number):
     generate_log(i+1, timestamp)
     timestamp += timedelta(seconds=2)
-
 
